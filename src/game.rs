e04ae0396947ae87d5389b7f2170321f73b07fed
Stephan Luther <kvsari@gmail.com>
2019-01-04 00:10:44 +0900

    Shift function into `game` module

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,20 @@
 //! Game code.
+use rand::prelude::*;
 
 pub mod hold;
+
+use crate::hexagon;
+
+/// Temporary function. Remove this with a properly wrapped up game tree. An intermediate
+/// step is to return a game turn that will wrap the `hold::Hold` allowing that sub-module
+/// to be made private.
+pub fn generate_random_2x2_board_game() -> hexagon::grid::Rectangular<hold::Hold> {
+    let mut rng = thread_rng();    
+    let grid = hexagon::grid::Rectangular::generate(2, 2, hold::Hold::default());
+    
+    grid.fork(move |_| {
+        let player_code = rng.gen_range(1, 3);
+        let player_dice = rng.gen_range(1, 6);
+        hold::Hold::new(player_code, player_dice)
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,11 @@
 
 extern crate dicey_dice_lib as lib;
 
-use rand::prelude::*;
-
-use lib::{hexagon, game};
-
-/// TODO: Move this into a `game` module or something.
-fn generate_random_2x2_board_game() -> hexagon::grid::Rectangular<game::hold::Hold> {
-    let mut rng = thread_rng();    
-    let grid = hexagon::grid::Rectangular::generate(2, 2, game::hold::Hold::default());
-    
-    grid.fork(move |_| {
-        let player_code = rng.gen_range(1, 3);
-        let player_dice = rng.gen_range(1, 6);
-        game::hold::Hold::new(player_code, player_dice)
-    })
-}
+use lib::game;
 
 fn main() {
     println!("Dicey Dice starting...");
 
-    //let grid = grid::Rectangular::generate(2, 2, "A4");
-    let grid = generate_random_2x2_board_game();
+    let grid = game::generate_random_2x2_board_game();
     println!("{}", &grid);
 }
