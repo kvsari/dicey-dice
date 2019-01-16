f59841b618bf095dfc17b12916909faa3251f808
Stephan Luther <kvsari@gmail.com>
2019-01-17 00:20:52 +0900

    WIP: Redesigning the `Players` struct
    Need to do this as the current state of the players must be stored
    with the board state. This is because players can be dropped out
    but the game continues. Also, it will ensure full game state is
    stored per board allowing arbitrary tree processing order.

--- src/game/player.rs
+++ src/game/player.rs
@@ -8,7 +8,7 @@ use rand::distributions::Distribution;
 
 const MAX_PLAYERS: usize = 8;
 
-/// Contains the current player.
+/// Describes a player.
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, Getters)]
 pub struct Player {
     number: usize,
@@ -39,20 +39,52 @@ impl fmt::Display for Player {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum State {
+    Present(Player),
+    Removed(Player),
+    Empty,
+}
+
+impl State {
+    fn new(player: Player) -> Self {
+        State::Present(player)
+    }
+
+    fn empty() -> Self {
+        State::Empty
+    }
+
+    fn remove(&self) -> Self {
+        match self {
+            State::Present(p) => State::Removed(p),
+            State::Removed(p) => State::Removed(p),
+            State::Empty => State::Empty,
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::empty()
+    }
+}
+
 /// Player management rolled into one struct. Keeps track of the current player and
 /// emits the next player. There is an upper limit of `MAX_PLAYERS` players.
 #[derive(Debug, Copy, Clone)]
 pub struct Players {
-    count: usize,
+    players: usize,
     current: usize,
-    players: [Player; MAX_PLAYERS],
+    playing: usize,
+    roster: [State; MAX_PLAYERS],
 }
 
 impl Players {
     /// If `players` is larger than `MAX_PLAYERS`, will truncate to `MAX_PLAYERS`. If
     /// `players` is less than 2, will use a minimum of 2.
     pub fn new(players: usize) -> Self {
-        let count = if players > MAX_PLAYERS {
+        let players = if players > MAX_PLAYERS {
             MAX_PLAYERS
         } else if players < 2 {
             2
@@ -60,21 +92,23 @@ impl Players {
             players
         };
 
+        let playing = players;
         let current = 0;
 
-        let mut players = [Player::default(); MAX_PLAYERS];
+        let mut roster = [State::default(); MAX_PLAYERS];
 
-        players
+        roster
             .iter_mut()
             .enumerate()
-            .for_each(|(index, player)| {
+            .for_each(|(index, state)| {
                 let character: char = ((65 + index) as u8).into();
-                let mut n_player = Player::new(index, character);
-                mem::swap(player, &mut n_player);
+                let player = Player::new(index, character);
+                let mut n_state = State::new(player);
+                mem::swap(state, &mut n_state);
             });
 
         Players {
-            count, current, players
+            players, current, playing, roster
         }
     }
 
@@ -82,13 +116,15 @@ impl Players {
         self.players[self.current]
     }
 
-    pub fn next(&mut self) -> Player {
+    /*
+    pub fn next(&self) -> Player {
         self.current += 1;
         if self.current >= self.count {
             self.current = 0;
         }
         self.current()
     }
+    */
 }
 
 impl Distribution<Player> for Players {
