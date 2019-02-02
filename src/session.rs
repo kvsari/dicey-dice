e7c31352b0b47e9e502f0019149c9b5eb121fe26
Stephan Luther <kvsari@gmail.com>
2019-02-03 00:26:54 +0900

    WIP: Trying new `Session` interface

--- src/session.rs
+++ src/session.rs
@@ -3,6 +3,89 @@ use derive_getters::Getters;
 
 use crate::game::{self, Tree, Board, Players, Player, Choice};
 
+/// The state of the session.
+#[derive(Debug, Clone)]
+pub struct State {
+    pub turn: usize,
+    pub board: Board,
+    pub choices: Vec<Choice>,
+}
+
+impl State {
+    fn new(turn: usize, board: Board, choices: &[Choice]) -> Self {
+        State {
+            turn,
+            board,
+            choices: choices.into_iter().map(|c| *c).collect(),
+        }
+    }
+}
+
+
+    
+/// A game in progress. The `traversals` indicate how many turns have passed. Maintains
+/// all state of the game.
+///
+/// ## Invariants
+/// 1. The `tree` will always be valid.
+/// 2. The first `Board` in the `traversal` is the starting position.
+/// 3. There will always be at least one `Board` in the `traversal`.
+#[derive(Debug, Clone, Getters)]
+pub struct Session {
+    traversal: Vec<State>,
+    tree: Tree,
+    choice: Option<Choice>,
+}
+
+impl Session {
+    pub fn new(start: Board, tree: Tree) -> Self {
+        Session {
+            traversal: vec![start],
+            tree,
+            choice: None,
+        }
+    }
+
+    pub fn reset(self) -> Self {
+        Session {
+            traversal: vec![self.traversal.first().take().unwrap().to_owned()],
+            tree: self.tree,
+            choice: None,
+        }
+    }
+            
+    //pub fn current(&self) -> 
+    
+    /*
+    /// Return the current `Board`.
+    pub fn board(&self) -> &Board {
+        self.traversal.last().unwrap()
+    }
+
+    /// Return the current `Player`. Can also be sourced from the `Board`.
+    pub fn player(&self) -> Player {
+        self.traversal.last().unwrap().players().current()
+    }
+
+    /// Choices available from the current `Board`.
+    pub fn choices(&self) -> &[Choice] {
+        self.tree.fetch_choices(self.board()).unwrap()
+    }
+
+    /// Lock in a `Choice`.
+    pub fn choose(&mut self, choice: Choice) -> Result<(), String> {
+        Err("Invalid choice.".to_owned())
+    }
+
+    /// Advance the state of the `Session`. If a choice has been made, will apply it. If
+    /// no choice has been made, perhaps the game can be advanced anyway such as a turn over
+    /// or a player `GameOver`. Returns `true` if the game advanced.
+    pub fn advance(&mut self) -> bool {
+        if 
+    }
+    */
+}
+
 /// Setup a game session. Can set the number of players and the board size and to use
 /// canned boards (feed it a starting position. The board can only be rectangular.
 #[derive(Debug, Clone, Getters)]
@@ -58,57 +141,4 @@ impl Default for Setup {
     fn default() -> Self {
         Setup::new(Players::new(2))
     }
-}
-
-/// A game in progress. The `traversals` indicate how many turns have passed. Maintains
-/// all state of the game.
-///
-/// ## Invariants
-/// 1. The `tree` will always be valid.
-/// 2. The first `Board` in the `traversal` is the starting position.
-/// 3. There will always be at least one `Board` in the `traversal`.
-#[derive(Debug, Clone, Getters)]
-pub struct Session {
-    traversal: Vec<Board>,
-    tree: Tree,
-    choice: Option<Choice>,
-}
-
-impl Session {
-    pub fn new(start: Board, tree: Tree) -> Self {
-        Session {
-            traversal: vec![start],
-            tree,
-            choice: None,
-        }
-    }
-
-    /// Return the current `Board`.
-    pub fn board(&self) -> &Board {
-        self.traversal.last().unwrap()
-    }
-
-    /// Return the current `Player`. Can also be sourced from the `Board`.
-    pub fn player(&self) -> Player {
-        self.traversal.last().unwrap().players().current()
-    }
-
-    /// Choices available from the current `Board`.
-    pub fn choices(&self) -> &[Choice] {
-        self.tree.fetch_choices(self.board()).unwrap()
-    }
-
-    /// Lock in a `Choice`.
-    pub fn choose(&mut self, choice: Choice) -> Result<(), String> {
-        Err("Invalid choice.".to_owned())
-    }
-
-    /*
-    /// Advance the state of the `Session`. If a choice has been made, will apply it. If
-    /// no choice has been made, perhaps the game can be advanced anyway such as a turn over
-    /// or a player `GameOver`. Returns `true` if the game advanced.
-    pub fn advance(&mut self) -> bool {
-        if 
-    }
-    */
 }
