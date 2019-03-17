60ebcf6fea3400bb58947fd08a3225b9bfdd045b
Stephan Luther <kvsari@gmail.com>
2019-03-17 21:30:42 +0900

    Enable wasm-bindgen feature on rand dep

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,17 @@ use lib::{session, game, console};
 fn main() -> Result<(), Box<dyn error::Error + 'static>> {
     println!("Dicey Dice starting...");
 
-    let players = game::Players::new(6);
-    let start = game::generate_random_board(10, 10, players);
+    let players = game::Players::new(2);
+    let start = game::generate_random_board(8, 8, players);
     
     //let start = game::canned_2x2_start04();
     //let start = game::canned_3x1_start05();
     //let start = game::canned_3x2_start02();
-    let start = game::canned_3x3_start03();
+    let start = game::canned_3x3_start01();
 
     println!("Using this board:\n{}", &start);
 
-    //let ai_player_a = game::Player::new(1, 'A');
+    let ai_player_a = game::Player::new(1, 'A');
     let ai_player_b = game::Player::new(2, 'B');
     let ai_player_c = game::Player::new(3, 'C');
     let ai_player_d = game::Player::new(4, 'D');
@@ -29,12 +29,12 @@ fn main() -> Result<(), Box<dyn error::Error + 'static>> {
     let ai_player_f = game::Player::new(6, 'F');
     let ai_players: HashSet<game::Player> = HashSet::from_iter(
         vec![
-            //ai_player_a,
+            ai_player_a,
             ai_player_b,
-            ai_player_c,
-            ai_player_d,
-            ai_player_e,
-            ai_player_f,
+            //ai_player_c,
+            //ai_player_d,
+            //ai_player_e,
+            //ai_player_f,
         ].into_iter()
     );
     
@@ -43,8 +43,8 @@ fn main() -> Result<(), Box<dyn error::Error + 'static>> {
         //.set_move_limit(NonZeroU8::new(2).unwrap())
         .session()?;
 
-    //console::play_session(session);
-    console::play_session_with_ai(session, ai_players, 20);
+    console::play_session(session);
+    //console::play_session_with_ai(session, ai_players, 4);
 
     Ok(())
 }
